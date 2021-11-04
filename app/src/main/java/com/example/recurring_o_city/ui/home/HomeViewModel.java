9a32f2b3f87972f9fa52611eb052c895324e9b29
hmhoangUA <78331484+hmhoangUA@users.noreply.github.com>
2021-11-04 11:03:24 -0600

    Added MainActivity and Side Menu

---
+++ app/src/main/java/com/example/recurring_o_city/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.recurring_o_city.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
