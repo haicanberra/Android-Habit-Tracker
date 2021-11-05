5bfabd02ca3be1bd29d298ee2e48fd07a3aa0411
pknguyenn <78003230+pknguyenn@users.noreply.github.com>
2021-11-05 11:12:55 -0600

    Update data to FireStore

--- app/src/main/java/com/example/recurring_o_city/TodayFragment.java
+++ app/src/main/java/com/example/recurring_o_city/TodayFragment.java
@@ -2,15 +2,28 @@ package com.example.recurring_o_city;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -19,8 +32,10 @@ import java.util.Date;
 
 public class TodayFragment extends Fragment{
 
-    private ArrayList<Habit> habitList;
+    public ArrayList<Habit> habitList;
+    public ItemAdapter habitAdapter;
     private FloatingActionButton fab;
+    FirebaseFirestore db;
 
     public TodayFragment() {
         // Required empty public constructor
@@ -50,10 +65,37 @@ public class TodayFragment extends Fragment{
 
         RecyclerView recyclerView = view.findViewById(R.id.today_recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
-        recyclerView.setAdapter(new ItemAdapter(habitList));
+        habitAdapter = new ItemAdapter(habitList);
+        recyclerView.setAdapter(habitAdapter);
+
+
+        db = FirebaseFirestore.getInstance();
+        CollectionReference collectionReference = db.collection("Habits");
 
         fab = view.findViewById(R.id.fab);
         fab.setOnClickListener(v -> new AddHabitFragment().show(getActivity().getSupportFragmentManager(), "ADD_HABIT"));
+
+
+//        db.collection("Users")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                Log.d("Get habit", "Get data successfully");
+//                            }
+//                        } else {
+//                            Log.d("Get habit", "Error getting documents: ", task.getException());
+//                        }
+//                    }
+//                });
+
+
+
+//                FragmentManager fm = getSupportFragmentManager();
+//                fragmentadapter = new FragmentAdapter(fm, getLifecycle(), habitList, habitEventList);
+//                pager2.setAdapter(fragmentadapter);
 
         return view;
     }
